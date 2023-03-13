f2dc8e89d443c0a4e2b00e292410e529889f2548
Alan Gibran <lushwacka@comunidad.unam.mx>
2023-03-13 09:10:06 -0600

    not so final version that has to be reviewed

--- src/hrapp/Department.java
+++ src/hrapp/Department.java
@@ -54,26 +54,51 @@ public class Department {
         }
         return actualEmployees;
     }
-    
-    public int getEmployeeCount(){
+
+    public int getEmployeeCount() {
         return lastAddedEmployeeIndex + 1;
     }
-    
+
     //Query method that attempts to locale an employee w a specific ID
-    public Employee getEmployeeByID(int employeeID){
-        for (Employee emp:employees){
-            if (emp.getID() == employeeID){
-                return emp;
+    public Employee getEmployeeByID(int employeeID) {
+        for (Employee emp : employees) {
+            if (emp != null) {
+                if (emp.getID() == employeeID) {
+                    return emp;
+                }
             }
         }
         return null;//Mandatory to return this bc ?¿
         /*ok we return a null just in case we dont find the ID*/
     }
+
     //Total salary for all employees
-    public double getTotalSalary(){
+    public double getTotalSalary() {
+        /*
+        Because employees array may be partially filled with values, you need
+        to start at position zero and stop at the element indicated by lastAd...
+        Calculate running total value and return the result.
+         */
+
+        double totalSalary = 0.0;
+        for (int i = 0; i <= lastAddedEmployeeIndex; i++) {
+            totalSalary += employees[i].getSalary();
+        }
+        return totalSalary;
+    }
+
+    public double getAverageSalary() {
+
+        /*To calculate the average salary i have to calculate th total and then
+        divide it by the total number of elements, but I already have a method 
+        that calculates it. So i just have to find the total number of elements
+        by adding +1 to the last... index variable.*/
+        if (lastAddedEmployeeIndex > -1) {
+            return getTotalSalary() / (lastAddedEmployeeIndex + 1);
+        }
         return 0.0;
     }
-    
+
     //Overriding toString()String
     @Override
     public String toString() {
--- src/hrapp/HRApp.java
+++ src/hrapp/HRApp.java
@@ -11,29 +11,36 @@ public class HRApp {
      */
     public static void main(String[] args) {
         System.out.println("HR App Starts");
-    
-    Employee e1 = new Employee("Yef", 1, 17500);
-    Employee e2 = new Employee("Krr", 2, 21000);
-    Employee e3 = new Employee("maik", 3, 0.0); 
-    Employee e4 = new Employee("Shirguis", 4, 0);
-    Employee e5 = new Employee("Loletita", 5, 10);
 
-    
+        Employee e1 = new Employee("Yef", 1, 17500);
+        Employee e2 = new Employee("Krr", 2, 21000);
+        Employee e3 = new Employee("maik", 3, 0.0);
+        Employee e4 = new Employee("Shirguis", 4, 0);
+        Employee e5 = new Employee("Loletita", 5, 10);
+
+        Employee[] employeeArray = {e1, e2, e3, e4, e5};
+
+        Department dept = new Department("Education");
+
+        dept.addEmployee(e1);
+        dept.addEmployee(e2);
+
+        dept.addEmployee(new Employee("Ao", 17, 17.0));
+
+        Employee[] emps = dept.getEmployees();
+
+        //The difference between these outputs
+        System.out.println("Emp" + e1);
         System.out.println(e1.toString());
-        System.out.println(e2.toString());
-        System.out.println(e3.toString());
-        System.out.println(e4.toString());
-        System.out.println(e5.toString());
 
-        
-    Employee[] employeeArray = {e1,e2,e3,e4,e5};
-
-    
-    
-    
-    /*int count = countNonNullElements(myArray); // get the number of non-null elements
+        for (Employee emp : emps) {
+            System.out.println("Emp_For " + emp);
+        }
+        System.out.println("Total " + dept.getTotalSalary());
+        System.out.println("Average Salary = " + dept.getAverageSalary());
+        System.out.println("By ID " + dept.getEmployeeByID(16));
+        /*int count = countNonNullElements(myArray); // get the number of non-null elements
     System.out.println(count);*/
-        
     }
-    
+
 }
